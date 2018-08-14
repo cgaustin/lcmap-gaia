50dbebf90b465d3e3b41de909694ddf1c17b0ac9
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-08-14 10:58:50 -0500

    switch from multi-method approach to using resolve when mapping products ns functions

--- src/lcmap/gaia/main.clj
+++ src/lcmap/gaia/main.clj
@@ -3,12 +3,10 @@
   (:require [clojure.tools.logging :as log]
             [lcmap.gaia.config     :as config]
             [lcmap.gaia.file       :as file]
-            [lcmap.gaia.products   :as products]
+            [lcmap.gaia.products   :refer :all]
             [lcmap.gaia.util       :as util]
             [mount.core            :as mount]
             [lcmap.gaia.gdal       :as gdal]))
-
-
 
 ;; ccdc results are the number of change segments
 ;; detected over a 100x100 pixel area (chip)
@@ -18,20 +16,12 @@
 ;; if no changes are detected, a result is 
 ;; returned with an sday and eday == 0
 
-
-(defmulti gen-product
-  (fn [infile product queryday] (keyword product)))
-
-(defmethod gen-product :default
-  [infile product queryday]
-  (log/infof "%s is not a valid product" product)
-  nil)
-
-(defmethod gen-product :time-of-change
+(defn gen-product
   [infile product queryday]
   (let [input (file/read-json infile)
+        product_fn (resolve (symbol product))
         pixel_segments (util/pixel-groups input)
-        pixel_array (map #(products/time-of-change (first %) (last %) queryday) pixel_segments)
+        pixel_array (map #(product_fn (first %) (last %) queryday) pixel_segments)
         output_name (products/product-name (first input) product "gtif")]
     ;(gdal/geotiff pixel_array output_name)
     (prn output_name)
