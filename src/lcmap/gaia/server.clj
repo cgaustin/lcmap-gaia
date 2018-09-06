8741072362c05fd230a8191082edb94a8fd7dd66
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-09-06 15:17:09 -0500

    wip server deliver tifs

--- src/lcmap/gaia/server.clj
+++ src/lcmap/gaia/server.clj
@@ -4,8 +4,57 @@
             [ring.middleware.json :as ring-json]
             [ring.middleware.keyword-params :as ring-keyword-params]
             [ring.middleware.defaults :as ring-defaults]
+            [ring.util.response :as ring-response]
             [org.httpkit.client :as http]
-            [org.httpkit.server :as http-server]))
+            [org.httpkit.server :as http-server]
+            [lcmap.gaia.file :as file]
+            [lcmap.gaia.products :as products]
+            [lcmap.gaia.gdal :as gdal]
+            [lcmap.gaia.util :as util])
+  (:import (java.io FileInputStream)))
+
+(def foo_data (file/read-json "resources/y3161805_x-2115585_nodates.json"))
+(def query_day "2006-07-01")
+
+(defmulti get-product
+  (fn [_ request] (-> request (:headers) (get "accept"))))
+
+(defmethod get-product :default
+  [product_type request]
+  {:status 200 :body [(str "please define a valid Accept header of either 'image/tif' or 'application/json'") ]})
+
+(defmethod get-product "image/tif"
+  [product_type request]
+  (let [input (file/read-json "resources/y3161805_x-2115585_nodates.json")
+        product_fn (-> (str "lcmap.gaia.products/" product_type) (symbol) (resolve))
+        product_values (products/data input product_fn query_day)
+        product_name (str (products/product-name (first input) product_type "tif"))
+        output_name (str "target/" product_name) 
+        chipx (get (first input) "chipx")
+        chipy (get (first input) "chipy")
+        proj_wkt (util/get-projection)]
+    (prn (str "count product_values: " (count product_values)) )
+    (gdal/geotiff_from_pixel_array product_values output_name chipx chipy proj_wkt)
+    ;{:status 200 :body product} length-of-segment_-2115585_3161805.tif
+    ;(-> product_name (ring-response/file-response {:root "target/"}) (ring-response/header "content-type" "image/tif") )
+    ;(-> product_name (ring-response/file-response "length-of-segment_-2115585_3161805.tif") (ring-response/header "content-type" "image/tif") ) 
+    {:status 200 :headers {"Content-Type" "image/tif"} :body (FileInputStream. output_name)}
+))
+
+(defmethod get-product "application/json"
+  [product_type request]
+  (let [input foo_data
+        product_fn (-> (str "lcmap.gaia.products/" product_type) (symbol) (resolve))
+        product_values (products/data input product_fn query_day)
+        chipx (get (first input) "chipx")
+        chipy (get (first input) "chipy")]
+    {:status 200 :body {"chipx" chipx "chipy" chipy "values" product_values}}))
+
+(defn serve-file 
+  [request]
+  {:status 200
+   :headers {"Content-Type" "image/tif"}
+   :body (FileInputStream. "length-of-segment_-2115585_3161805.tif")})
 
 (defn healthy
   "Hello Gaia"
@@ -15,7 +64,9 @@
 (compojure/defroutes routes
   (compojure/context "/" request
                      (route/resources "/")
-                     (compojure/GET "/" [] (healthy request))))
+                     (compojure/GET "/" [] (healthy request))
+                     (compojure/GET "/foo" [] (serve-file request))
+                     (compojure/GET "/product/:product_type" [product_type] (get-product product_type request))))
 
 (defn response-handler
   [routes]
