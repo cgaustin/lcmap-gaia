e0577ddf4c7fecc989d3bd659546b9a8a702d519
caustin-usgs <cgaustin@contractor.usgs.gov>
2018-11-01 14:25:46 -0500

    fix util tests

--- test/lcmap/gaia/util_test.clj
+++ test/lcmap/gaia/util_test.clj
@@ -43,17 +43,17 @@
   (let [map_a {:foo true :bar false}
         map_b {:foo false :bar true}
         expected_return [map_a map_b]]
-    (is (= (products/matching-keys map_a map_b :foo :bar true) expected_return))))
+    (is (= (util/matching-keys map_a map_b :foo :bar true) expected_return))))
 
 (deftest matching-keys-return-first-map-test
   (let [map_a {:foo true :bar false}
         map_b {:foo false :bar true}
         map_coll [map_a]]
-    (is (= (products/matching-keys map_coll map_b :foo :bar true) map_coll))))
+    (is (= (util/matching-keys map_coll map_b :foo :bar true) map_coll))))
 
 (deftest matching-keys-return-last-map-test
   (let [map_a {:foo true :bar false}
         map_b {:foo false :bar true}]
-    (is (= (products/matching-keys map_a map_b :foo :bar false) map_b))))
+    (is (= (util/matching-keys map_a map_b :foo :bar false) map_b))))
 
 
