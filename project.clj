eb63f35da6c08c2bc1c2351ac50e216eb5107c20
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-02-05 18:50:45 -0600

    s3 style object storage interface WIP

--- project.clj
+++ project.clj
@@ -27,7 +27,8 @@
                  [ring/ring-jetty-adapter   "1.6.3"]
                  [ring/ring-mock            "0.3.2"]
                  [org.gdal/gdal             "2.2.0"]
-                 [mount                     "0.1.12"]]
+                 [mount                     "0.1.12"]
+                 [amazonica                 "0.3.139"]]
 
   :plugins [[lein-environ "1.1.0"]]
   :profiles {:dev     {:dependencies [[org.clojure/test.check "0.9.0"]]}
--- src/lcmap/gaia/storage.clj
+++ src/lcmap/gaia/storage.clj
@@ -1,10 +1,16 @@
 (ns lcmap.gaia.storage
-  (:require [lcmap.gaia.config :refer [config]]))
+  (:require [lcmap.gaia.config :refer [config]]
+            [amazonica.aws.s3 :as s3]))
 
+(def client-config
+  {:access-key (:s3-access-key config)
+   :secret-key (:s3-secret-key config)
+   :endpoint   (:s3-endpoint config)
+   :client-config {:path-style-access-enabled true}})
 
 (defn list_buckets
   []
-  true)
+  (s3/list-buckets client-config))
 
 (defn create_bucket
   [name]
@@ -22,3 +28,22 @@
   [filename data]
   (let [output (str (:output_path config) filename)]
     (spit output data)))
+
+
+;; s3test.core=> (def x (assoc cred :client-config {:path-style-access-enabled true}))
+;; #'s3test.core/x
+;; s3test.core=> (s3/list-buckets x)
+;; [{:creation-date #object[org.joda.time.DateTime 0x224b55a0 "2018-12-17T08:06:24.547-06:00"], :owner {:display-name "cgaustin", :id "cgaustin"}, :name "clayfoo"}]
+;; s3test.core=> 
+
+
+;; s3test.core=> (s3/create-bucket x "jack")
+;; {:name "jack"}
+;; s3test.core=> (s3/list-buckets x)
+
+
+;; s3test.core=> (def upload-file (java.io.File. "upload.txt"))
+;; #'s3test.core/upload-file
+;; s3test.core=> (spit upload-file "hello world")
+;; nil
+;; s3test.core=> (s3/put-object x :bucket-name "jack" :key "jenny" :file upload-file)
