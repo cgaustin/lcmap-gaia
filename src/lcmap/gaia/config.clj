24d687bb118ea2873680d14d5493f647a86893cc
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-04-05 14:27:00 -0500

    dont set a default for storage bucket

--- src/lcmap/gaia/config.clj
+++ src/lcmap/gaia/config.clj
@@ -27,7 +27,7 @@
    :predictions_path   (:predictions-path         environ/env)
    :storage-access-key (:storage-access-key       environ/env)
    :storage-secret-key (:storage-secret-key       environ/env)
-   :storage-bucket     (or (:storage-bucket       environ/env) "lcmap-tile")
+   :storage-bucket     (:storage-bucket           environ/env)
    :storage-endpoint   (or (:storage-endpoint     environ/env) "http://localhost:7480")
    :query_day          (or (:query-day            environ/env) "07-01")
    :http_port      (or (try-read (:http-port      environ/env)) 9876)
