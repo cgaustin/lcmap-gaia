d882a45cc65fffd1dfad6e70efd9500bcb2e2b6b
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-04-11 10:15:08 -0500

    add chipmunk tests

--- test/lcmap/gaia/chipmunk_test.clj
+++ test/lcmap/gaia/chipmunk_test.clj
@@ -8,7 +8,8 @@
 (def nlcd-cx 111111)
 (def nlcd-cy 222222)
 (def nlcd-url (str "http://localhost:5656/chips?ubid=AUX_NLCD&x=" nlcd-cx "&y=" nlcd-cy "&acquired=1999-01-01/2002-01-01"))
-(def nlcd-vals [41 52 63])
+(def nlcd-vals [41 52 0])
+(def nlcd-mask-vals [1 1 0])
 
 (defn nlcd_resp []
   (let [byte-vals (byte-array nlcd-vals)
@@ -24,4 +25,22 @@
     (let [response (chipmunk/nlcd nlcd-cx nlcd-cy)]
       (is (= response nlcd-vals)))))
 
+(deftest test-binary
+  (is (= 1 (chipmunk/binary 55)))
+  (is (= 0 (chipmunk/binary 0))))
 
+(deftest test-nlcd_mask
+  (with-fake-http [{:url nlcd-url :method :get} {:status 200 :body (nlcd_resp)}]
+    (let [response (chipmunk/nlcd_mask nlcd-cx nlcd-cy)]
+      (is (= response nlcd-mask-vals))))
+  (is (= nlcd-mask-vals (chipmunk/nlcd_mask nlcd-vals))))
+
+(deftest test-nlcd_filters
+  (with-fake-http [{:url nlcd-url :method :get} {:status 200 :body (nlcd_resp)}]
+    (let [response (chipmunk/nlcd_filters nlcd-cx nlcd-cy)]
+      (is (= (:values response) nlcd-vals))
+      (is (= (:mask response) nlcd-mask-vals))))
+
+
+
+)
