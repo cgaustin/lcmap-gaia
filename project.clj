82d8f63c34ad255a48349b9c39ac004e7664ef53
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-05-01 07:35:11 -0500

    v 0.3.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gaia "0.3.10"
+(defproject gaia "0.3.11"
   :description "Level 2 CCDC Product Generation"
   :url "http://github.com/usgs-eros/lcmap-gaia"
   :license {:name "Unlicense"
