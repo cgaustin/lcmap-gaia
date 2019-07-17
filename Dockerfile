731c0237b3a4a9cbc78264b76e8ef063dfdc8c6d
caustin-usgs <cgaustin@contractor.usgs.gov>
2019-07-17 13:04:36 -0500

    add generate cog fn

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ RUN apt-get install openjdk-8-jre curl vim -y
 RUN apt-get install gdal-bin libgdal-dev libgdal-java libgdal20 python-gdal -y
 COPY resources/log4j.properties /log4j.properties
 COPY target/gaia-*-standalone.jar /
+ENV GTIFF_IGNORE_READ_ERRORS True
 CMD java -server -Xms$Xms -Xmx$Xmx -XX:+UseG1GC -jar gaia-*-standalone.jar
 
--- src/lcmap/gaia/gdal.clj
+++ src/lcmap/gaia/gdal.clj
@@ -2,7 +2,7 @@
   (:require [mount.core :as mount]
             [lcmap.gaia.util :as util]
             [clojure.tools.logging :as log]
-            [clojure.java.shell :only [sh]])
+            [clojure.java.shell :refer [sh]])
   (:import [org.gdal.gdal gdal]
            [org.gdal.gdal Driver]
            [org.gdal.gdal Dataset]
@@ -104,16 +104,23 @@
   ([tiff_name values x_offset y_offset]
    (update_geotiff tiff_name values x_offset y_offset 100 100)))
 
-;```This is how I've done it in the past - replace {} with the tif filename - the {}_tiled_cloud.TIF is your final file:
-;/usr/local/bin/gdal_translate {}.TIF {}_tiled.TIF -co TILED=YES -co COMPRESS=DEFLATE; 
-;/usr/local/bin/gdaladdo -ro -r average {}_tiled.TIF 2 4 8; 
-;/usr/local/bin/gdal_translate {}_tiled.TIF {}_tiled_cloud.TIF -co TILED=YES -co COMPRESS=DEFLATE -co COPY_SRC_OVERVIEWS=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 --config GDAL_TIFF_OVR_BLOCKSIZE 512```
 (defn generate-cog
   [input output]
-  (let [step1 (format "/usr/local/bin/gdal_translate %s %s -co TILED=YES -co COMPRESS=DEFLATE" input output)
-        step2 (format "/usr/local/bin/gdaladdo -ro -r average %s 2 4 8" output)
-        step3 (format "/usr/local/bin/gdal_translate %s %s -co TILED=YES -co COMPRESS=DEFLATE -co COPY_SRC_OVERVIEWS=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 --config GDAL_TIFF_OVR_BLOCKSIZE 512" output output)])
+  (try
+   (let [step1 (sh "gdal_translate" input "working.tif" "-co" "TILED=YES" "-co" "COMPRESS=DEFLATE")
+         step2 (sh "gdaladdo" "-ro" "-r" "average" "working.tif" "2" "4" "8")
+         step3 (sh "gdal_translate" "working.tif" output "-co" "TILED=YES" "-co" 
+                   "COMPRESS=DEFLATE" "-co" "COPY_SRC_OVERVIEWS=YES" "-co" "BLOCKXSIZE=512" "-co" 
+                   "BLOCKYSIZE=512" "--config" "GDAL_TIFF_OVR_BLOCKSIZE" "512")
+         failed (filter (fn [i] (= 1 (:exit i))) [step1 step2 step3])]
+     
+     (if (empty? failed)
+       output
+       (let [errors (map :err failed)
+             msg (format "problem with gdal utilities: %s" errors)]
+         (throw (ex-info msg {:type "data-generation-error"})))))
+  (catch Exception e
+    (let [msg (format "problem generating COG - input: %s  output: %s, message: %s" input output (.getMessage e))]
+      (log/error msg)
+      (throw (ex-info msg {:type "data-generation-error" :message msg} (.getCause e)))))))
 
-
-
-  true)
